aa50d431f7a698aed6b7679f06a3cd3fe90bad00
muyeyong <xuyongshuaige@gmail.com>
2020-04-29 20:38:17 +0800

    2020/04/29

--- src/pages/charts/bar.jsx
+++ src/pages/charts/bar.jsx
@@ -9,8 +9,6 @@ import { reqCategorys, reqWoList } from '../../api';
 export default class Bar extends Component {
 
     state = {
-        sales: [5, 20, 36, 10, 10, 20], // 销量的数组
-        stores: [6, 10, 25, 20, 15, 10], // 库存的数组,
         categorys: [],
         wosNamber: []
     }
@@ -25,62 +23,57 @@ export default class Bar extends Component {
         if (result.status === 0) {
             let categorys = result.data;
             let categorysId = categorys && categorys.map(item => item._id);
-            console.log(categorysId)
-            let wosNumber = await reqWoList(JSON.stringify(categorysId));
-            console.log(wosNumber)
-
+            let categorysName = categorys && categorys.map(item => item.name)
+            result = await reqWoList(JSON.stringify(categorysId));
+            if (result.status === 0) {
+                let wosNumber = result.data;
+                this.setState({
+                    categorys: categorysName,
+                    wosNamber: wosNumber
+                })
+            }
         }
     }
 
 
-    update = () => {
-        this.setState(state => ({
-            sales: state.sales.map(sale => sale + 1),
-            stores: state.stores.reduce((pre, store) => {
-                pre.push(store - 1)
-                return pre
-            }, []),
-        }))
-    }
+
 
     /*
     返回柱状图的配置对象
      */
-    getOption = (sales, stores) => {
+    getOption = () => {
+        const { wosNamber, categorys } = this.state;
         return {
             title: {
-                text: 'ECharts 入门示例'
+                text: '订单数据'
             },
             tooltip: {},
             legend: {
-                data: ['销量', '库存']
+                data: ['订单数']
             },
             xAxis: {
-                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                data: categorys
             },
-            yAxis: {},
+            yAxis: {
+                type: 'value'
+            },
             series: [{
-                name: '销量',
+                name: '总订单数',
                 type: 'bar',
-                data: sales
-            }, {
-                name: '库存',
-                type: 'bar',
-                data: stores
+                data: wosNamber,
+                showBackground: true,
+                backgroundStyle: {
+                    color: 'rgba(220, 220, 220, 0.8)'
+                }
             }]
         }
     }
 
     render() {
-        const { sales, stores } = this.state
         return (
             <div>
-                <Card>
-                    <Button type='primary' onClick={this.update}>更新</Button>
-                </Card>
-
                 <Card title='柱状图一'>
-                    <ReactEcharts option={this.getOption(sales, stores)} />
+                    <ReactEcharts option={this.getOption()} />
                 </Card>
 
             </div>
